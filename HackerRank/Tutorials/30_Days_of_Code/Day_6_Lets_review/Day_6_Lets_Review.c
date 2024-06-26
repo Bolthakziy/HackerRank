d37730d3f5f8af64d83576dae80a6e73b04be58d
Alec K <boltakziy@gmail.com>
2024-06-26 22:37:30 +0900

    Day 6 : Let's Review.

--- HackerRank/Tutorials/30_Days_of_Code/Day_6_Lets_review/Day_6_Lets_Review.c
+++ HackerRank/Tutorials/30_Days_of_Code/Day_6_Lets_review/Day_6_Lets_Review.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
+int main() {
+	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+	int T, i;
+	char* S;
+
+	scanf("%d", &T);
+	while(T--) {
+		S = (char*)malloc(sizeof(char) * 10000);
+		scanf("%s", S);
+
+		for(i = 0; *(S + i) != '\0'; i++) {
+			if((i & 1) == 0) {
+				printf("%c", *(S + i));
+			} else {
+
+			}
+		}
+		printf(" ");
+		for(i = 0; *(S + i) != '\0'; i++) {
+			if((i & 1) != 0) {
+				printf("%c", *(S + i));
+			} else {
+
+			}
+		}
+		printf("\n");
+
+		free(S);
+	}
+
+	return 0;
+}
